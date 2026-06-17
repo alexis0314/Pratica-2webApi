660b1b64947e8077ab4ddf62b64f8d2950350059
alexis0314 <alexismercedes365@gmail.com>
2026-06-17 18:18:48 -0400

    .

---
+++ Pratica#2webApi/Controllers/EstudianteController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pratica_2webApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Pratica_2webApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstudianteController : ControllerBase
+    {
+        private static List<Estudiante> estudiantes = new List<Estudiante>();
+
+        // GET: api/<EstudianteController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<EstudianteController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EstudianteController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<EstudianteController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EstudianteController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Pratica#2webApi/Models/Estudiante.cs
@@ -0,0 +1,19 @@
+﻿namespace Pratica_2webApi.Models
+{
+    public class Estudiante
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo    { get; set; }
+        public string Carrera { get; set; }
+
+        public int Edad { get; set; }
+
+        public decimal Promedio { get; set; }
+
+        public bool Activo { get; set; }
+    }
+
+    
+}
